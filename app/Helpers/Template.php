0775bf95d35bb94eaf8097762beb329f15fc2836
ph03022903 <phuocndtkw@gmail.com>
2022-08-13 11:29:23 +0700

    update_ header _title

--- app/Helpers/Template.php
+++ app/Helpers/Template.php
@@ -2,18 +2,18 @@
 namespace App\Helpers;
 use Config;
 class Template {
-    public static function HeaderTitleContent($title,$breadcrumb)
+    public static function HeaderTitleContent($title,$link)
     {
         $html = sprintf('
         <div class="row page-titles">
             <div class="col-md-5 align-self-center">
                 <h3 class="text-themecolor">%s</h3>
             </div>
-            <div class="col-md-7 align-self-center">
-                %s
+            <div class="col-md-7 align-self-center text-right">
+            <a type="button" href="%s" class="btn btn-primary"><i class="fa fa-plus"></i> Thêm mới</a>
             </div>
         </div>
-        ',$title,$breadcrumb);
+        ',$title,$link);
         return $html;
     }
 
